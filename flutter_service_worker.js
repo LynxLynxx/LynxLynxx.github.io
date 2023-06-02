44e151154b243c6f4da74f3912737f13b24ca914
Ryszard Schossler <ryszard.schossler@gmail.com>
2023-06-02 11:24:49 +0200

    adding responsiveness to pages. Make resume site

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,18 +4,17 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "03acefc4795e8573b194262cd3a4419f",
-"index.html": "69d19c4decbbee3751451872f65d1d51",
-"/": "69d19c4decbbee3751451872f65d1d51",
-"main.dart.js": "389cf4b31cab26ca79ba9e337403b1f1",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"index.html": "56a69163c357ecb130c04de2e3c7d17b",
+"/": "56a69163c357ecb130c04de2e3c7d17b",
+"main.dart.js": "e49a2d7f158b78c3e5952574f7440f99",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "493757defcfefaf370ef90ea0954c227",
-"assets/AssetManifest.json": "51e1d9f1629940024bab9b2109d0ca00",
-"assets/NOTICES": "91f01b35853cb328b7656a2e678d660c",
+"assets/AssetManifest.json": "4ab1b22350386d2c6989f41d64c623cc",
+"assets/NOTICES": "aa69d341c3f9b6271c8845be99a5ca1c",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "9cda082bd7cc5642096b56fa8db15b45",
@@ -23,13 +22,10 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b00363533ebe0bfdb95f3694d7647f6d",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/img/me.png": "8803d93e7314cdce5c59bd91ce14799b",
+"assets/assets/img/pluginIcon.svg": "305c0bb9897a4b13c0f30e6fe0e748ac",
 "assets/assets/img/linkedin_icon.png": "8c54498de170f54d31a75a7bb0e6c998",
 "assets/assets/img/pobrane.jpeg": "2b88e689613de3a31bb550e38fb6b1ed",
-"assets/assets/img/github_icon.png": "add631b638f2680caf976d349e2db7e0",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/img/github_icon.png": "add631b638f2680caf976d349e2db7e0"
 };
 
 // The application shell files that are downloaded before a service worker can
